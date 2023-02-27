00d1707b62c2c5dab18b34f303b5fb63906cca86
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-02-27 04:46:34 -0800

    Task 12 added

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,44 @@
 #include "main.h"
 
 /**
- * print_number - prints and integer
+ * print_number - prints an integer
  * @n: the integer
- * Return: 0 success
  */
-int main(void)
+void print_number(int n)
 {
-	long int number = 612852475143;
-	long int largest_prime = number;
-	long int track_factor = 2;
+	int num_of_digits = 0, clone_n = n,
+	    first_digit_of_n, digits_of_n = 1, original_n;
 
-	while (track_factor < largest_prime)
+	original_n = n;
+	if (n == 0)
 	{
-		if (largest_prime % track_factor == 0)
-		{
-			largest_prime /= track_factor;
-		}
-		else
-		{
-			track_factor++;
-		}
+		_putchar('0');
+		return;
 	}
-	printf("%li\n", largest_prime);
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (n * -1) - 1;
+	}
+	while (clone_n != 0)
+	{
+		num_of_digits++;
+		clone_n /= 10;
+	}
+
+	while (digits_of_n <= num_of_digits)
+	{
+		clone_n = n;
+		first_digit_of_n = digits_of_n;
+		while (first_digit_of_n < num_of_digits)
+		{
+			clone_n /= 10;
+			first_digit_of_n++;
+		}
+		if (original_n < 0 && digits_of_n == num_of_digits)
+			_putchar(((clone_n % 10) + 48) + 1);
+		else
+			_putchar((clone_n % 10) + 48);
+		digits_of_n++;
+	}
 }
