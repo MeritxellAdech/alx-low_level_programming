c1638990bad670b78f36c7bba86a06b19b8d26af
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-02-28 12:24:05 -0800

    Task 10 added

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: is the string to be converted to integer
+ * Return: success 0
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
