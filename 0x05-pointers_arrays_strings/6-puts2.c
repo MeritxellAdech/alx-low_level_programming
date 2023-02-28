1d642473a66ccc9a7c85c44c403a0a6d08ad2654
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-02-27 21:40:51 -0800

    Task 6 added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: is the string
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+		len++;
+	}
+	_putchar('\n');
+}
