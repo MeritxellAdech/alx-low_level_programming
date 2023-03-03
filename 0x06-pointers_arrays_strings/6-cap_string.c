8c99144cf9a33929f04b264132bb557c9a7c820c
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-02 19:36:32 -0800

    Modify task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,29 @@
 char *cap_string(char *s)
 {
 	int index = 0;
-	char delimiters[] = ",;.!?\"(){}\t\n ";
-	int del_index;
+	char delimiters[] = ",;.!?\"(){}\n \t";
+	unsigned int del_index;
 
 	while (s[index] != '\0')
 	{
 		del_index = 0;
-		while (del_index < (sizeof(delimiters) / sizeof(char)))
+		while (del_index < (sizeof(delimiters) / sizeof(char)) - 1)
 		{
 			if (s[index] == delimiters[del_index])
 			{
+				if (s[index] == '\t')
+					s[index] = ' ';
 				if (s[index + 1] >= 'a' && s[index + 1] <= 'z')
-					s[index] = s[index] - 32;
-				del_index++;
-				break;
+				{
+					s[index + 1] = s[index + 1] - 32;
+					index++;
+					break;
+				}
 			}
+			del_index++;
 		}
 		index++;
 	}
+
+	return (s);
 }
