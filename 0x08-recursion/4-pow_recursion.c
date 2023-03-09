d12e66d547d2517824380646bf8fea16b954203b
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-08 22:13:33 -0800

    add tak 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,13 +11,13 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 1)
-	{
-		return (-1);
-	}
-	else if (y == 0)
+	if (y == 0)
 	{
 		return (1);
+	}
+	else if (y < 1)
+	{
+		return (-1);
 	}
 	else if (y > 1)
 	{
