20f92b127d2d894d91095f3c70fa7a281ab1945c
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-08 22:00:08 -0800

    add task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * find_sqrt -finds the square for the given number
+ * beginning at the natural number @i
+ *
+ * @n: is the given number
+ * @i:is the natural numbers starting from 1
+ * until the square root is found
+ *
+ * Return: the square root for the number.
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the square root 
+ * for the given number
+ *
+ * @n: the given number
+ *
+ * Return: the sqrt of @n
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_sqrt(n, 1));
+}
