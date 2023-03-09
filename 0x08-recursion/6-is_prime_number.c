7a36491031ad2cd560cde5327887dd431a2d2c57
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-08 21:54:46 -0800

    add task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 is a number
+ * is prime and 0 if it is not
+ *
+ * @n: it is the number to check against
+ *
+ * Return: prime as 1 or complex as 0
+ */
+int is_prime_number(int n)
+{
+	int factors = n, prime;
+
+	if (n < 4)
+	{
+		return 1;
+	}
+	if (factors <= n)
+	{
+		prime = n / is_prime_number(factors - 1);
+		return 1;
+	}
+	return 0;
+}
