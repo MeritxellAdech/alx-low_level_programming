7196b4b4f7be15874f45a99baf4b00b4dad0608c
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-08 23:36:51 -0800

    update task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ *prime_finder - Search for the next divisor of @n
+ * @n: is the number we are checking against
+ *
+ * @inc: integer to move through all natural
+ * numbers
+ *
+ * Return: returns 1 if @n is prime otherwise 0
+ */
+int prime_finder(int n, int inc)
+{
+	if (inc == n)
+	{
+		return (1);
+	}
+	else if (n % inc == 0 && inc < n)
+	{
+		return (0);
+	}
+	return (prime_finder(n, inc + 1));
+}
+
+/**
  * is_prime_number - returns 1 is a number
  * is prime and 0 if it is not
  *
@@ -10,16 +32,9 @@
  */
 int is_prime_number(int n)
 {
-	int prime, factors = n;
-
-	if (n < 4)
+	if (n < 2)
 	{
-		return (1);
+		return (0);
 	}
-	if (factors <= n)
-	{
-		prime = n / is_prime_number(factors - 1);
-		return (1);
-	}
-	return (0);
+	return (prime_finder(n, 2));
 }
