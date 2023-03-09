d49673940b452e83860ca9732cb51afb860942ec
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-08 20:26:03 -0800

    add task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of @x raised to
+ * the power of @y
+ *
+ * @x: is the given number
+ * @y: is the power to which @x is to be raised
+ *
+ * Return: the result after exponentiation
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 1)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y >)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (x);
+}
