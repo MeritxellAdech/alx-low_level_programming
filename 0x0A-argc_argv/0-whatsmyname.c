aea85dfeab5b5d0ccce036fbefd818e7ae436f4d
Meritxell Cheba Ade <meritxelladech@gmail.com>
2023-03-10 18:41:46 -0800

    add task 0 of 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Print program name
+ * @argc: Number of arguments
+ * @argv: Values of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+
+	return (0);
+}
