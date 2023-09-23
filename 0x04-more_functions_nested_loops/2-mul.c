5059b35455947678a872bd76778e3785b2adb49d
Merit <meritxelladech@gmail.com>
2023-09-23 13:35:47 -0700

    add a function that returns the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ *
+ * @a: is the first number
+ * @b: is the second number
+ * Return: the product on success otherwise 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
