bfc7ab7d0d0436eb8a8443499ede8993793fe7fe
Merit <meritxelladech@gmail.com>
2023-10-22 18:20:03 +0000

    Update: add constructor function that prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * constructor - prints a string before main function
+ * is executed
+ */
+void constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
