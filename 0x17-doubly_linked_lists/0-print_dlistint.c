3f5c6fc26e9f74cf0fd28dec3ab3207cef5a4711
Merit <meritxelladech@gmail.com>
2024-03-21 13:58:44 +0000

    ADD: header file with needed prototypes and structs

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements in a list
+ *
+ * @h: given list
+ * Return: the number of found elements or -1
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
